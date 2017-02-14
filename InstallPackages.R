4191bab8385b13c1f5f8cc2f2067a9184cb71969
kellobri <kellyobriant@gmail.com>
2017-02-13 21:06:22 -0500

    Use shiny-dev function to solve package issue

--- InstallPackages.R
+++ InstallPackages.R
@@ -1,3 +1,8 @@
 # Install Packages
 
 install.packages(c('twitteR','tm','SnowballC','stringr','wordcloud'))
+
+# Use the Add packages function from shiny-dev
+# addPackages <- function(pkgs) {
+#  install.packages(pkgs, '/usr/lib64/R/library')
+# }
